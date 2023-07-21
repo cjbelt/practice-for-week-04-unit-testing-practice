58d78d744e2f4e296269bc577d672b351ea4b54d
cjbelt <cjbeltran@protonmail.com>
2023-07-21 16:19:42 -0300

    funcs and class specs added

--- funcs.js
+++ funcs.js
@@ -1,13 +1,32 @@
 function isFive(num) {
   // Your code here
+  if (num === 5) {
+    return true;
+  }
+
+  return false;
 }
 
 function isOdd(number) {
   // Your code here
+  if (typeof number !== "number") {
+    throw new TypeError("The argument must be a number");
+  } else if (number % 2 !== 0) {
+    return true;
+  }
+
+  return false;
 }
 
 function myRange(min, max, step = 1) {
   // Your code here
+  const newArr = [];
+
+  for (let i = min; i <= max; i += step) {
+    newArr.push(i);
+  }
+
+  return newArr;
 }
 
 
--- test/class-spec.js
+++ test/class-spec.js
@@ -4,31 +4,60 @@ const expect = chai.expect;
 const { Word } = require("../class");
 
 describe("Word", function () {
+  let word;
+
+  before(() => {
+    word = new Word("testing");
+  });
   describe("Word constructor function", function () {
+
     it('should have a "word" property', function () {
-      expect.fail("replace with your code");
+      expect(word.word).to.exist;
     });
-  
+
     it('should set the "word" property when a new word is created', function () {
-      expect.fail("replace with your code");
+      expect(word.word).to.equal("testing");
     });
   });
 
   describe("removeVowels function", function () {
     it("should return a the word with all vowels removed", function () {
-      expect.fail("replace with your code");
+      const newWord = word.removeVowels();
+      expect(newWord).to.deep.equal("tstng");
+
+      const fullVowel = new Word("aeiouAEIOU");
+      const empty = fullVowel.removeVowels();
+      expect(empty).to.deep.equal("");
+
+      const noVowels = new Word("try");
+      const tryWord = noVowels.removeVowels();
+      expect(tryWord).to.deep.equal("try");
     });
   });
 
   describe("removeConsonants function", function () {
     it("should return the word with the consonants removed", function () {
-      expect.fail("replace with your code");
+      const newWord = word.removeConsonants();
+      expect(newWord).to.deep.equal("ei");
+
+      const fullConsonant = new Word("bbbcccddd");
+      const empty = fullConsonant.removeConsonants();
+      expect(empty).to.deep.equal("");
+
+      const noConsonants = new Word("aeiou");
+      const vowels = noConsonants.removeConsonants();
+      expect(vowels).to.deep.equal("aeiou");
     });
   });
-  
+
   describe("pigLatin function", function () {
     it("should return the word converted to pig latin", function () {
-      expect.fail("replace with your code");
+      const newWord = word.pigLatin();
+      expect(newWord).to.deep.equal("estingtay");
+
+      const firstVowel = new Word("aether");
+      const pigLatined = firstVowel.pigLatin();
+      expect(pigLatined).to.deep.equal("aetheryay");
     });
   });
 });
